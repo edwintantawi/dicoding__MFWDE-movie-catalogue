a6f083b9b22c82569f036e2f03ced383ca7b65aa
edwintantawi <edwintantawi@gmail.com>
2021-04-30 15:47:53 +0700

    render data from api, and added minifest.json

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,16 +1,18 @@
 import UrlParser from '../../routes/url-parser';
 import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
-      <h2>Now Playing</h2>
+      <div id="movie" class="movie"></div>
     `;
   },
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
-    const movie = await TheMovieDbSource.detailMovie(url);
-    console.info(movie);
+    const movie = await TheMovieDbSource.detailMovie(url.id);
+    const movieContainer = document.querySelector('#movie');
+    movieContainer.innerHTML += createMovieDetailTemplate(movie);
   },
 };
 
--- src/scripts/views/pages/upcoming.js
+++ src/scripts/views/pages/upcoming.js
@@ -1,14 +1,21 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieItemTemplate } from '../templates/template-creator';
 
 const UpComing = {
   async render() {
     return `
-      <h2>Up Coming</h2>
+      <div class="content">
+        <h2 class="content__heading">Upcoming in Cinema</h2>
+        <div id="movies" class="movies"></div>
+      </div>
     `;
   },
   async afterRender() {
     const movies = await TheMovieDbSource.upComingMovies();
-    console.info(movies);
+    const movieContainer = document.querySelector('#movies');
+    movies.forEach((movie) => {
+      movieContainer.innerHTML += createMovieItemTemplate(movie);
+    });
   },
 };
 
